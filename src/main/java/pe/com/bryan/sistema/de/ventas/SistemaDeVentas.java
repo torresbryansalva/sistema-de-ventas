7f1ef4d80d1a59089333b236ed8eabbc8d6237d5
Bryan Salva <salvatorresb@gmail.com>
2022-10-20 15:12:55 -0500

    archivos base del proyecto

---
+++ src/main/java/pe/com/bryan/sistema/de/ventas/SistemaDeVentas.java
@@ -0,0 +1,15 @@
+
+package pe.com.bryan.sistema.de.ventas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class SistemaDeVentas {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola listo para programar?");
+    }
+}
